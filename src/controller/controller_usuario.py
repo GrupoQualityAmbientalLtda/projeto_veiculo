bbd78411c4b75c5b22cad0e77820536f71cef93f
Grupo Quality Ambiental <ti@grupoqualityambiental.com.br>
2025-04-09 11:52:04 -0300

    controller usuario + dao_usuario consertado

--- src/controller/controller_usuario.py
+++ src/controller/controller_usuario.py
@@ -1,6 +1,7 @@
 from src.dao.dao_usuario import DaoUsuario
 from src.models.usuario import Usuario
 from src.database.db import create_session
+import pandas as pd
 
 class ControllerUsuario:
     @classmethod
@@ -32,11 +33,39 @@ class ControllerUsuario:
                                      permissao = permissao,
                                      status = status)
             session.commit()
-            return True # Por que os return Trues?
+            return True
         
         except Exception as e:
             session.rollback()
             print(f"Erro gerado {e}")
             return False
         finally:
-            session.close()        
+            session.close()
+    
+    @classmethod
+    def obter_usuarios(cls):
+        with create_session() as session:
+            try:
+                usuarios = DaoUsuario.listar_todos(session)
+                return usuarios
+            except Exception as e:
+                return []
+    @classmethod
+    def listar_usuarios(cls):
+        with create_session() as session:
+            usuarios = cls.obter_usuarios
+            lista_usuarios = []
+            for usuario in usuarios:
+                lista_usuarios.append((usuario.id, usuario.nome, usuario.login, usuario.permissao, usuario.status))
+            return lista_usuarios
+    @classmethod
+    def atualizar_usuario_pelo_id(cls, id, novo_nome, novo_login, nova_permissao, novo_status):
+        with create_session() as session:
+            try:
+                DaoUsuario.atualizar_usuario_pelo_id(session, id, novo_nome, novo_login, nova_permissao, novo_status)
+                session.commit()
+                return True
+            except Exception as e:
+                print(f'Erro gerado {e}')
+                session.rollback()
+                return None
--- src/dao/dao_usuario.py
+++ src/dao/dao_usuario.py
@@ -12,5 +12,17 @@ class DaoUsuario:
     def obter_usuario(cls, session, id):
         usuario = session.query(Usuario).filter(Usuario.id == id).first()
         return usuario
-
     
+    @classmethod
+    def listar_todos(cls, session):
+        usuario = session.query(Usuario).order_by(Usuario.id).all()
+        return usuario
+    
+    @classmethod
+    def atualizar_usuario_pelo_id(cls, session, id, novo_nome, novo_login, nova_permissao, novo_status):
+        usuario = session.query(usuario).filter(usuario.id == id).first()
+        usuario.nome = novo_nome
+        usuario.status = novo_status
+        usuario.login = novo_login
+        usuario.permissao = nova_permissao
+        return usuario
